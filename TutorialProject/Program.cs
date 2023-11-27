4b99da34555ee4e518a9ffe74617d59a420ed8f0
bakhanh113ct <84719735+bakhanh113ct@users.noreply.github.com>
2023-11-27 13:00:02 +0700

    first commit

---
+++ TutorialProject/Program.cs
@@ -0,0 +1,55 @@
+using Microsoft.EntityFrameworkCore;
+using TutorialProject.Data;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<ApplicationDbContext>();
+
+
+//MSSQL DB Conn:
+//string connString = builder.Configuration.GetConnectionString("DefaultConnectionMSSQLNoCred");
+//builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connString));
+
+
+//MySQL DB Conn
+var _GetConnStringName = builder.Configuration.GetConnectionString("DefaultConnectionMySQL");
+//var builder = WebApplication.CreateBuilder(args);
+
+// Connect to database
+builder.Services.AddDbContext<ApplicationDbContext>(option =>
+{
+    option.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnectionMySQL"));
+});
+//builder.Services.AddDbContextPool<ApplicationDbContext>(options => options.UseMySql(_GetConnStringName, ServerVersion.AutoDetect(_GetConnStringName)));
+
+
+
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
